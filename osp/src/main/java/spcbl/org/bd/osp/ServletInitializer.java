72288d841995b18a31cddc01cadcb8761ce8ce85
Prasun Kanti Howlader <prasunhowlader@yahoo.com>
2022-09-14 12:03:41 +0600

    Project Initialization

---
+++ osp/src/main/java/spcbl/org/bd/osp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package spcbl.org.bd.osp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OspApplication.class);
+    }
+
+}
