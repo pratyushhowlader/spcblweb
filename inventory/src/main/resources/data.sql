c8c36021373bc71ff6579432fea252e76c0bee4b
Prasun Kanti Howlader <prasunhowlader@yahoo.com>
2022-10-12 08:59:39 +0600

    Header file updated

--- inventory/src/main/resources/data.sql
+++ inventory/src/main/resources/data.sql
@@ -4,5 +4,14 @@
  * All rights reserved
  */
 
+insert into spcbl_inventory.device_category(category_name) values ('Computer');
 insert into spcbl_inventory.device_category(category_name) values ('Laptop');
+insert into spcbl_inventory.device_category(category_name) values ('Printer');
+insert into spcbl_inventory.device_category(category_name) values ('Scanner');
+insert into spcbl_inventory.device_category(category_name) values ('Switch');
+insert into spcbl_inventory.device_category(category_name) values ('Projector');
+insert into spcbl_inventory.device_category(category_name) values ('UPS');
+insert into spcbl_inventory.device_category(category_name) values ('Scanner');
+insert into spcbl_inventory.device_category(category_name) values ('Wireless Keyboard');
+insert into spcbl_inventory.device_category(category_name) values ('Mouse');
 /*insert into device_info(commen)*/
