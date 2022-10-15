49d518c209896b8c1daa6d93d47b26cfc0d32987
Prasun <prasunhowlader@yahoo.com>
2022-10-15 13:16:20 +0600

    New LocalDate implemented

--- inventory/src/main/java/spcbl/org/inventory/config/WebSecurityConfig.java
+++ inventory/src/main/java/spcbl/org/inventory/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package spcbl.org.inventory.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -40,8 +42,10 @@ public class WebSecurityConfig {
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.authenticationProvider(authenticationProvider()).authorizeRequests()
-                .antMatchers("/resources/**").permitAll()
+        http
+                .authenticationProvider(authenticationProvider()).authorizeRequests()
+               /* .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()*/
+                .antMatchers("/resources/**","/resources/","/fonts/**").permitAll()
                 .antMatchers("/").hasAnyAuthority("USER", "CREATOR", "EDITOR", "ADMIN")
                 .anyRequest().authenticated()
                 .and()
@@ -63,4 +67,8 @@ public class WebSecurityConfig {
         webSecurity.ignoring().antMatchers("/resources/**");
 
     }*/
+ /* @Bean
+  public WebSecurityCustomizer ignoringCustomizer() {
+      return (web) -> web.ignoring().antMatchers("/resources/**");
+  }*/
 }
