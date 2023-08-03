01b73bd678007a295abed1f4071af24ac0c4f8e8
Prasun Kanti Howlader <prasunhowlader@yahoo.com>
2023-08-03 11:48:32 +0600

    Header file updated

--- inventory/src/main/java/spcbl/org/inventory/InventoryApplication.java
+++ inventory/src/main/java/spcbl/org/inventory/InventoryApplication.java
@@ -2,7 +2,7 @@ package spcbl.org.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//testing
 @SpringBootApplication
 public class InventoryApplication {
 
